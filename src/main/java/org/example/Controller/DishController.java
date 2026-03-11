4b5449decd40bf75ed2b092c3a40eed76991ce2a
247626 Baska Dawid <247626@edu.p.lodz.pl>
2026-03-11 19:34:31 +0100

    DishController

---
+++ src/main/java/org/example/Controller/DishController.java
@@ -0,0 +1,54 @@
+package org.example.Controller;
+
+import org.example.Model.Dish;
+import org.example.Service.DishService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/dish")
+public class DishController {
+
+    private final DishService dishService;
+
+    @Autowired
+    public DishController(DishService dishService) {
+        this.dishService = dishService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Dish>> getAllDishes() {
+        List<Dish> dishList = dishService.readAllDishes();
+        return new ResponseEntity<>(dishList, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Dish> createDish(@RequestBody Dish dish) {
+        Dish dishDB = dishService.createDish(dish);
+        return new ResponseEntity<>(dishDB, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Dish> deleteDish(@PathVariable long id) {
+        try {
+            Dish dishDB = dishService.deleteDish(id);
+            return new ResponseEntity<>(dishDB, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Dish> updateDish(@PathVariable long id, @RequestBody Dish dish) {
+        try {
+            Dish dishDB = dishService.updateDish(id, dish);
+            return new ResponseEntity<>(dishDB, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
---
+++ src/test/java/org/example/Controller/DishControllerTest.java
@@ -0,0 +1,74 @@
+package org.example.Controller;
+
+import jakarta.persistence.EntityManager;
+import org.example.Model.Dish;
+import org.example.Model.DishType;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
+public class DishControllerTest {
+
+    @Autowired
+    private DishController dishController;
+
+    @Autowired
+    private EntityManager entityManager;
+
+
+    @Test
+    public void getTest(){
+        Dish dish = new Dish("Pizza", "ser, szynka, pieczarki", 50.50, DishType.MAIN);
+        Dish dish2 = new Dish("Sok pomarańczowy", "250 ml", 15, DishType.DRINK);
+        entityManager.persist(dish);
+        entityManager.persist(dish2);
+
+        ResponseEntity<List<Dish>> response = dishController.getAllDishes();
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+        Assertions.assertEquals(2, response.getBody().size());
+    }
+
+    @Test
+    public void postTest(){
+        Dish dish = new Dish("Pizza", "ser, szynka, pieczarki", 50.50, DishType.MAIN);
+
+        ResponseEntity<Dish> response = dishController.createDish(dish);
+
+        Assertions.assertEquals(HttpStatus.CREATED, response.getStatusCode());
+    }
+
+    @Test
+    public void putTest(){
+        Dish dish = new Dish("Pizza", "ser, szynka, pieczarki", 50.50, DishType.MAIN);
+        entityManager.persist(dish);
+
+        dish.setName("Sok");
+        ResponseEntity<Dish> response = dishController.updateDish(dish.getId(), dish);
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+
+        response = dishController.updateDish(-1L, dish);
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+
+    @Test
+    public void deleteTest(){
+        Dish dish = new Dish("Pizza", "ser, szynka, pieczarki", 50.50, DishType.MAIN);
+        entityManager.persist(dish);
+
+        ResponseEntity<Dish> response = dishController.deleteDish(dish.getId());
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+
+        response = dishController.deleteDish(-1L);
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+}
