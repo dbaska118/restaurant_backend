4ff785d46345e6bdbff8731feb55698dd0e67f9c
Dawid Baska <dbaska118@gmail.com>
2026-06-25 15:39:37 +0200

    ReservationRepository - metoda pobrania najblizszych rezerwacji dla stolików

--- src/main/java/org/example/repository/reservation/ReservationRepository.java
+++ src/main/java/org/example/repository/reservation/ReservationRepository.java
@@ -4,6 +4,8 @@ import org.example.model.reservation.Reservation;
 import org.example.model.reservation.ReservationStatus;
 import org.example.model.reservation.RestaurantTable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -15,4 +17,13 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
     List<Reservation> findAllByEmailAndReservationStatus(String email, ReservationStatus reservationStatus);
 
     List<Reservation> findByRestaurantTableAndStartTimeBeforeAndEndTimeAfter(RestaurantTable restaurantTable, LocalDateTime end, LocalDateTime start);
+
+
+    @Query(value = "SELECT DISTINCT ON (restaurant_table_id) * FROM reservation " +
+            "WHERE start_time >= :bufferTime AND start_time <= :endOfDay " +
+            "AND reservation_status = :status " +
+            "ORDER BY restaurant_table_id, start_time ASC",
+            nativeQuery = true)
+    List<Reservation> findNextReservations(@Param("bufferTime") LocalDateTime bufferTime, @Param("endOfDay") LocalDateTime endOfDay, @Param("status") ReservationStatus status);
+
 }
---
+++ src/test/java/org/example/repository/reservation/ReservationRepositoryTest.java
@@ -0,0 +1,55 @@
+package org.example.repository.reservation;
+
+
+import org.example.model.reservation.Reservation;
+import org.example.model.reservation.ReservationStatus;
+import org.example.model.reservation.RestaurantTable;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+public class ReservationRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private ReservationRepository reservationRepository;
+
+
+    @Test
+    public void findNextReservationsTest() {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime endDay = now.with(LocalTime.MAX);
+
+        RestaurantTable restaurantTable = new RestaurantTable("Stolik 1", 5);
+        RestaurantTable restaurantTable2 = new RestaurantTable("Stolik 2", 3);
+        entityManager.persist(restaurantTable);
+        entityManager.persist(restaurantTable2);
+
+
+        Reservation reservation = new Reservation("text@wp.pl", restaurantTable, now.plusHours(4), now.plusHours(6), 100, "000001", ReservationStatus.CONFIRMED);
+        Reservation reservation2 = new Reservation("client@wp.pl", restaurantTable, now, now.plusHours(2), 100, "000002", ReservationStatus.CONFIRMED);
+        Reservation reservation3 = new Reservation("client@wp.pl", restaurantTable2, now, now.plusHours(2), 100, "000003", ReservationStatus.CANCELLED);
+        entityManager.persist(reservation);
+        entityManager.persist(reservation2);
+        entityManager.persist(reservation3);
+
+        List<Reservation> reservations = reservationRepository.findNextReservations(now.minusHours(1), endDay, ReservationStatus.CONFIRMED);
+        Assertions.assertEquals(1, reservations.size());
+        Assertions.assertEquals(reservation2, reservations.get(0));
+
+
+    }
+}
